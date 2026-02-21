4bf1d5717bb3a8e133731e04fd842bbcff3e7edd
pigxity <87405798+pigxity@users.noreply.github.com>
2026-02-21 00:40:50 -0500

    default_list argument

--- api/arguments.py
+++ api/arguments.py
@@ -2,6 +2,7 @@ from exceptions import ParseError
 
 #[val1, val2, val3]
 def list_arg(string: str) -> list[str]:
+    """Converts a string wrapped in [] and seperated by commas into a python list"""
     if not (string.startswith('[') and string.endswith(']')):
         raise ParseError('list argument must be wrapped in `[` and `]` characters')
     string = string[1:-1]
@@ -9,6 +10,13 @@ def list_arg(string: str) -> list[str]:
         return []
 
     return [s.strip() for s in string.split(',')]
+
+def default_list(str: str):
+    """Tries to call list_arg, but if the argument cannot be parsed into a list, a list wrapped with the string is returned."""
+    try:
+        return list_arg(str)
+    except ParseError:
+        return [str]
 
 def literal_builder(options: list[str]):
     def f(s: str):
